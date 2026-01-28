9301ed05259fc787e80ff848d7cef33e902de160
PavanKuruva <pavankumar630492@gamil.com>
2026-01-28 14:35:56 +0530

    name-pwd

---
+++ login.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "Enter username:"
+READ USERNAME
+echo "Username is: $USERNAME"
+echo "Enter password:"
+READ Passwd
+echo "Entered password will not be visible now:"
+READ -s password
